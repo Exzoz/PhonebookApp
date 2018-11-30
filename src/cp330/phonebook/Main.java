0573be8d31df60f9573199c46b3f654c08b5a979
Exzoz <farisgjaleel@gmail.com>
2018-11-30 16:04:30 -0600

    updates to Main class

--- src/cp330/phonebook/Main.java
+++ src/cp330/phonebook/Main.java
@@ -7,6 +7,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(final String[] args) throws FileNotFoundException {
         System.out.println("Welcome to Phonebook");
-
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.help();
+        final Scanner input = new Scanner(System.in);
+        while (input.hasNext()) {
+            String line = input.nextLine();
+            phoneBook.processCommand(line);
+        }
     }
 }
